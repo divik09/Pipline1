6e0a4cab23d9a7a734ec7624613b2d520b4f610b
Divik Tiwari <51364181+divik09@users.noreply.github.com>
2021-05-04 06:14:33 +0530

    Update TestController.java

--- src/main/java/com/pipline/PipelineEx/TestController.java
+++ src/main/java/com/pipline/PipelineEx/TestController.java
@@ -8,6 +8,6 @@ public class TestController {
     
     @GetMapping()
     public String test() {
-       return "<h1 align="center">This is Testing Website Working Correctly !!!</h1>";
+       return "This is Testing Website Working Correctly";
     }
 }
